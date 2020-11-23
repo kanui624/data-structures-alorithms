1b4237a4ef4a0e00cf2b7efd42669ba690ef4b47
Kanui <marley.kanui@gmail.com>
2020-11-23 15:40:27 -0600

    data-structures-algorithms - global commit 08 - Added constant time complexity example

--- big-o-notation/timeComplexity.js
+++ big-o-notation/timeComplexity.js
@@ -75,4 +75,17 @@ const logAtLeast5 = (n) => {
 
 logAtLeast5(6);
 
-// Constant Example:
+// Constant Example: O(1)
+
+console.log(
+  '#####################################################################'
+);
+console.log('Linear Example: O(1)');
+
+const logAtMost5 = (n) => {
+  for (let i = 1; i <= Math.min(5, n); i++) {
+    console.log(i);
+  }
+};
+
+logAtMost5(6);
