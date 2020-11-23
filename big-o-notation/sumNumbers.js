b2d0f1d61978b39723a01ae11120f9790b3012af
Kanui <marley.kanui@gmail.com>
2020-11-23 13:22:03 -0600

    data-structures-algorithms - global commit 02 - Running through sumUP algorithm example

---
+++ big-o-notation/sumNumbers.js
@@ -0,0 +1,26 @@
+// Prompt:
+// Write a function that calculates the sum of all numbers from 1 up to
+// and including some number n.
+
+// Corner Cases:
+// return message for integers less than 1
+// return message for invalid type
+
+// Pseudo Code:
+// if number is less than 1 and if typeof number === "number"
+// create a for loop that starts at 1 and loop n times
+// create a running sum and add next integer to the running sum each iteration
+
+const sumUp = (n) => {
+  if (n < 1 || typeof n !== 'number') {
+    console.log(`Argument: ${n} is invalid`);
+  } else {
+    let runningSum = 0;
+    for (let i = 1; i <= n; i++) {
+      runningSum += i;
+    }
+    console.log(runningSum);
+  }
+};
+
+const printSum = sumUp(5);
