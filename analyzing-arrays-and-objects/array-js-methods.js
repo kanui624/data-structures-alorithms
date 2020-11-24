43d8df4c6c0f8a637e62771f317664cab1fee020
Kanui <marley.kanui@gmail.com>
2020-11-24 10:55:24 -0600

    data-structures-algorithms - global commit 22 - Defined big O of appending an element to the beginning of an array

--- analyzing-arrays-and-objects/array-js-methods.js
+++ analyzing-arrays-and-objects/array-js-methods.js
@@ -4,13 +4,24 @@ let names = ['Marley', 'Isaac', 'Kanui'];
 console.log(names[2]);
 // Outputs:
 // Kanui
-// O(1) - This runs in constant time, because as long as you specify a
-// valid index the corresponding value will be fetched in constant time
+// O(1) Time - This runs in constant time, because as long as you
+// specify a valid index the corresponding value will be fetched in
+// constant time
 
 // Appending element to the end of an array
-const namePush = names.push('gerald');
+const namePushEnd = names.push('Gerald');
 
 console.log(names[3]);
 // Outputs:
 // Gerald
-// O(1) - Appending to the end of an array runs in constant time
+// O(1) Time - Appending to the end of an array runs in constant time
+
+// Appending to the beginning of an array
+const namePushBeginning = names.unshift('Jobin');
+
+console.log(names[0]);
+// Outputs:
+// Jobin
+// O(n) Time - Appending to the beginning of an array runs in linear
+// time because the 0 index is now mapped to a different value forcing
+// every other element in the array to be reassigned a new index
