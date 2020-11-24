7db148b482f827553350054af9383ab4e6320a1c
Kanui <marley.kanui@gmail.com>
2020-11-24 10:47:24 -0600

    data-structures-algorithms - global commit 21 - Defined big O of appending to the end of an array

--- analyzing-arrays-and-objects/array-js-methods.js
+++ analyzing-arrays-and-objects/array-js-methods.js
@@ -1,8 +1,16 @@
+// Accessing a Certain index in an Array:
 let names = ['Marley', 'Isaac', 'Kanui'];
 
-// Accessing a Certain index in an Array:
 console.log(names[2]);
 // Outputs:
 // Kanui
 // O(1) - This runs in constant time, because as long as you specify a
 // valid index the corresponding value will be fetched in constant time
+
+// Appending element to the end of an array
+const namePush = names.push('gerald');
+
+console.log(names[3]);
+// Outputs:
+// Gerald
+// O(1) - Appending to the end of an array runs in constant time
