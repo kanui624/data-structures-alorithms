4ba5e3ee83055f18828976e8a3dfad176ecc3ffd
Kanui <marley.kanui@gmail.com>
2020-11-23 23:22:02 -0600

    data-structures-algorithms - global commit 19 - Defined big O of accessing a array value with a specific index

--- analyzing-arrays-and-objects/array-js-methods.js
+++ analyzing-arrays-and-objects/array-js-methods.js
@@ -1,1 +1,8 @@
 let names = ['Marley', 'Isaac', 'Kanui'];
+
+// Accessing a Certain index in an Array:
+console.log(names[2]);
+// Outputs:
+// Kanui
+// O(1) - This runs in constant time, because as long as you specify a
+// valid index the corresponding value will be fetched in constant time
