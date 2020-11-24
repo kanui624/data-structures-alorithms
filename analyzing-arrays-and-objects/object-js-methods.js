be2ee041b8bdf22a372dc4f2598b050168a3996d
Kanui <marley.kanui@gmail.com>
2020-11-23 21:33:53 -0600

    data-structures-algorithms - global commit 15 - Implemented Keys method and defined it's time complexity

---
+++ analyzing-arrays-and-objects/object-js-methods.js
@@ -0,0 +1,16 @@
+let instructor = {
+  firstName: 'Marley',
+  isInstructor: true,
+  favoriteNumbers: [1, 2, 3, 4],
+};
+
+// Keys method:
+const keys = Object.keys(instructor);
+
+console.log(keys);
+// Outputs:
+// ['firstName', 'isInstructor', 'favoriteNumbers'];
+// Time Complexity:
+// O(N) Linear Time, because as the object grows the .keys() method
+// will traverse the extent of the object while adding the key names
+// to the output array
