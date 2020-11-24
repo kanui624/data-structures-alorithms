2ef4ce00472790aa1a87c2b32c07b94f6465d05c
Kanui <marley.kanui@gmail.com>
2020-11-23 21:46:36 -0600

    data-structures-algorithms - global commit 17 - Implemented Entries method and defined it's time complexity

--- analyzing-arrays-and-objects/object-js-methods.js
+++ analyzing-arrays-and-objects/object-js-methods.js
@@ -14,3 +14,28 @@ console.log(keys);
 // O(N) Linear Time, because as the object grows the .keys() method
 // will traverse the extent of the object while adding the key names
 // to the output array
+
+// Values Object Method:
+const values = Object.values(instructor);
+
+console.log(values);
+// Outputs:
+// [ 'Marley', true, [ 1, 2, 3, 4 ] ]
+// O(N) Linear Time, because similar to the .keys() method, as the
+// object grows the .values() method will traverse the extent of the
+// object while adding the value names to the output array
+
+// Entries Object Method:
+const entries = Object.entries(instructor);
+
+console.log(entries);
+// Outputs:
+// [
+//   [ 'firstName', 'Marley' ],
+//   [ 'isInstructor', true ],
+//   [ 'favoriteNumbers', [ 1, 2, 3, 4 ] ]
+// ]
+// O(N) Linear Time, because similar to the .keys() and .values()
+// methods, as the object grows the .entries() method will traverse the
+// extent of the object while adding the key/value pairs to the output
+// array
