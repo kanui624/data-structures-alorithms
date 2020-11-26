fa32c08662ee58df64147353da15ba6895819192
Kanui <marley.kanui@gmail.com>
2020-11-26 11:26:15 -0600

    data-structures-algorithms - global commit 34 - Solving example problem for divide and conquer pattern

---
+++ problem-solving-approach/divide-conquer.js
@@ -0,0 +1,27 @@
+// Divide and Conquer
+// This pattern involves dividing a data set into smaller chunks and then
+// repeating a process with a subset of data
+
+// This pattern can tremendously decrease time complexity
+
+// Example:
+// Prompt:
+// Given a sorted array of integers, write a function called search, that accepts
+// a value and returns the index where the value passed to the function is located.
+// If the value is not found, return -1
+
+// Restate:
+// accept a sorted array and a value, and find the index in the array in which
+// that integer is located
+// Inputs:
+// a SORTED array and a positive or negative integer
+// Outputs:
+// the index at which the value was found or -1 to indicate that the value was
+// not found
+// Edge Cases:
+// what if the input array.length is 0?
+// Test Cases:
+// divideConquer([1,2,3,4,5,6,7,8,9], 5) => return 6
+// divideConquer([-8,-3,-1, 0], 2) => return -1
+
+// Pseudo Code:
