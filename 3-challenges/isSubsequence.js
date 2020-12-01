87aa7c3e7a32cc52e90894b21d4a41608df2c129
Kanui <marley.kanui@gmail.com>
2020-12-01 09:49:00 -0600

    data-structures-algorithms - global commit 48 - Added priliminary notes on isSubsequence challenge

---
+++ 3-challenges/isSubsequence.js
@@ -0,0 +1,28 @@
+// Multiple Pointers - isSubsequence
+// Prompt:
+// Write a function called isSubsequence which takes in two strings and checks whether the
+// characters in the first string form a subsequence of the second string. In other words, the
+// function should check whether the characters in the first string appear somewhere in the
+// second string, without their order changing.
+
+// Restate:
+// Write a function that takes in two strings and check to see if the first string is a
+// subsequence of the second string. return a boolean specifying if so or not
+
+// Input:
+// two strings
+
+// Outputs:
+// Boolean
+
+// Edge Cases:
+// Can one character be considered and subSequence?
+// Is it only a subsequence if it's characters are in the same order as the comparing string?
+
+// Test I/O:
+// isSubsequence("hello", "hello world") => true
+// isSubsequence("sing", "string") => true
+// isSubsequence("abc", "y a slkdj b sdklj c ") => true
+// isSubsequence("acb", "abc") => false
+
+//
