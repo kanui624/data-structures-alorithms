41380ccb1184559b2816b7f83326096248be1472
Kanui <marley.kanui@gmail.com>
2020-12-01 00:25:52 -0600

    data-structures-algorithms - global commit 42 - Annotated Instructor's first solution

--- 3-challenges/same-frequency.js
+++ 3-challenges/same-frequency.js
@@ -83,3 +83,34 @@ console.log(sameFrequency(182, 382));
 //   accessing an index in a string runs in O(1) constant time because at
 //   string construction every character in the string is treated like a
 //   value in an array and is given a index number
+
+// Instructor Solution:
+
+function sameFrequency(num1, num2) {
+  // Convert Both argument to strings so they get assigned indices and we can calculate their
+  // length
+  let strNum1 = num1.toString();
+  let strNum2 = num2.toString();
+  // Check corner case: If the two argument are not of the same length then return false
+  if (strNum1.length !== strNum2.length) return false;
+  // Initialize two empty arguments to push the contents of both arguments into respectively
+  let countNum1 = {};
+  let countNum2 = {};
+  // Start a for loop to assign and tally up instances and their duplicates if any in strNum1
+  // in countNum1
+  for (let i = 0; i < strNum1.length; i++) {
+    countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1;
+  }
+  // Start a for loop to assign and tally up instances and their duplicates if any in strNum2
+  // in countNum2
+  for (let j = 0; j < strNum1.length; j++) {
+    countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1;
+  }
+  // start another for loop to check if the same key value pairs match in both objects in
+  // frequency, if they don't return false
+  for (let key in countNum1) {
+    if (countNum1[key] !== countNum2[key]) return false;
+  }
+  // if we pass through everything return true because everything checks out
+  return true;
+}
