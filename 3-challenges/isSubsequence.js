06d8084090e87277dc4984c449405e2c0567625e
Kanui <marley.kanui@gmail.com>
2020-12-01 09:56:43 -0600

    data-structures-algorithms - global commit 49 - Added pseudo code to isSubsequence challenge

--- 3-challenges/isSubsequence.js
+++ 3-challenges/isSubsequence.js
@@ -15,7 +15,7 @@
 // Outputs:
 // Boolean
 
-// Edge Cases:
+// Clarify:
 // Can one character be considered and subSequence?
 // Is it only a subsequence if it's characters are in the same order as the comparing string?
 
@@ -25,4 +25,17 @@
 // isSubsequence("abc", "y a slkdj b sdklj c ") => true
 // isSubsequence("acb", "abc") => false
 
-//
+// Corner Case:
+// Check if seqPoint is equal to sequence.length if so we have reached the end of our sequence
+// meaning all characters check out
+
+//Pseudo Code
+// initialize a pointer strPoint at 0
+// initialize a pointer seqPoint at 0
+// while strPoint is less than string.length and while seqPoint less than sequence.length
+// check if seqPoint === sequence.length
+// if so return true
+// else check if string[strPoint] === sequence[seqPoint]
+// if so increment both pointers up
+// if not only increment the string pointer to check the next character
+// if we exit the while loop having never returned true then return false
