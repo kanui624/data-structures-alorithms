ae8b93aa70148ee152e96f5774c4738162a51b06
Kanui <marley.kanui@gmail.com>
2020-12-05 01:53:34 -0600

    data-structures-algorithms - global commit 69 - Wrote notes for productOfArray recursive challenge

---
+++ 4-recursion/0-recursion-challenges/productOfArray.js
@@ -0,0 +1,18 @@
+// Recursion - Product of Array
+// Prompt:
+// Write a function called productOfArray which takes in an array of integers
+// and returns the product of them.
+
+// Restate:
+// Write a function that takes in an array and returns the product of all
+// elements in the array
+// Inputs:
+// an array of integers
+// Outputs:
+// An integer representative of the product of the array
+// Test I/O:
+// productOfArray([1, 2, 3, 4]) => 24
+// productOfArray([1, 2, 3, 4, 5]) => 120
+// productOfArray([1, 2, 3, -4]) => -24
+// Edge Cases:
+// What if the array's length is 0? => return 1
