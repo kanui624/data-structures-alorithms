132e14dbb7e378ef773085f580b567544df4d988
Kanui <marley.kanui@gmail.com>
2020-12-05 01:42:46 -0600

    data-structures-algorithms - global commit 67 - starting second recursive challenge

---
+++ 4-recursion/0-recursion-challenges/factorial.js
@@ -0,0 +1,18 @@
+// Recursion - Factorial
+// Write a function which accepts a number and returns the factorial of that
+// number. A factorial is the product of an integer and all the integers below it
+// Example: Factorial 4 => 24 because 4 * 3 * 2 * 1. Factorial 0 is always 1
+
+// Restate:
+// Write a function that takes in an integer and returns the factorial of that
+// integer. Handle this recursively
+// Inputs:
+// Single positive integer
+// Outputs:
+// Single positive integer
+// Test I/O:
+// factorial(3) => 6
+// factorial(4) => 24
+// factorial(5) => 120
+// Edge Cases:
+// If the integer is 0 return 1
