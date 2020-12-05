655039b2f62e315dc84b822d73353059460c0110
Kanui <marley.kanui@gmail.com>
2020-12-05 02:00:42 -0600

    data-structures-algorithms - global commit 71 - Wrote solution productOfArray recursive challenge

--- 4-recursion/0-recursion-challenges/productOfArray.js
+++ 4-recursion/0-recursion-challenges/productOfArray.js
@@ -20,3 +20,10 @@
 // Pseudo Code:
 // if array.length === 0 return 1
 // return array[0] * productOfArray(array.slice(1))
+
+const productOfArray = (array) => {
+  if (array.length === 0) return 1;
+  return array[0] * productOfArray(array.slice(1));
+};
+
+console.log(productOfArray([1, 2, 3, 4, 5]));
