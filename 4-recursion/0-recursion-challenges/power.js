9e4029624838b2b17bebd527f5545e1fd3f8de6c
Kanui <marley.kanui@gmail.com>
2020-12-04 22:57:30 -0600

    data-structures-algorithms - global commit 65 - Doing first recursive challenge

---
+++ 4-recursion/0-recursion-challenges/power.js
@@ -0,0 +1,22 @@
+// Power
+// Prompt:
+// Write a function which accepts a base and an expononent. The function should
+// return the power of the base to the exponent. This function should mimic the
+// functionality of Math.pow() - do not worry about negative bases and exponents.
+
+// Restate:
+// Write a function which accepts a non-negative base and exponent. Return the
+// power of the base to the exponent.
+// Inputs:
+// two integers
+// Outputs:
+// an integer
+// Test I/O:
+// power(2,0) => 1
+// power(2,2) => 4
+// power(2, 4) => 16
+// Edge Cases:
+// If the argument passed in as the base equals 0 return 0
+// If the argument passed in as the base equals 1 return 1
+// If the argument passed in as the exponent equals 0 return 0
+// If the argument passed in as the exponent equals 1 return the base
