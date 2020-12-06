ca1fe42c12631989dbeb12e8e65afb5960f9968a
Kanui <marley.kanui@gmail.com>
2020-12-06 15:46:43 -0600

    data-structures-algorithms - global commit 72 - Wrote notes for rteverse recursive challenge

---
+++ 4-recursion/0-recursion-challenges/reverse.js
@@ -0,0 +1,18 @@
+// Reverse a string - Recursive Challenge
+// Prompt:
+// Write a function called reverse which accepts a string and returns a reversed
+// version of that string
+
+// Restate:
+// Write a function that accepts a string and returns the reverse version of that
+// string
+// Inputs:
+// a String
+// Outputs:
+// A reversed version of the input string
+// Test I/O:
+// reverse("hey") => "yeh"
+// reverse("abcdefghjk") => "kjhgfedcba"
+// reverse("oock") => "kcoo"
+// Edge Cases:
+// What if the string's length is 0? => return ""
