0ebab4a42664a27f01c86c71f2b970a9bc806d13
Kanui <marley.kanui@gmail.com>
2020-12-07 08:58:10 -0600

    data-structures-algorithms - global commit 80 - Wrote notes for flatten recursive challenge

---
+++ 4-recursion/0-recursion-challenges/flatten.js
@@ -0,0 +1,16 @@
+// Flatten - Recursion
+// Prompt:
+// Write a function called flatten which acceptd an array of arrays a returns a new
+// array with all values flattened
+
+// Restate:
+// Given a multi-dimensional array, consolidate or "flatten" all values in each array
+// into one array
+// Inputs:
+// Multi-dimensional array
+// Outputs:
+// An array
+//Test I/O:
+// flatten([1, 2, 3, [4, 5, 6, [7, 8], 8], 9]) => [1, 2, 3, 4, 5, 6, 7, 8, 8, 9]
+// Edge Cases:
+// If empty array return empty array
