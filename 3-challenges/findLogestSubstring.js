da446de026c47864a5fa65b27aff93c8d1061fd8
Kanui <marley.kanui@gmail.com>
2020-12-07 13:28:23 -0600

    data-structures-algorithms - global commit 86 - Wrote notes for sliding window findLongestSubstring challenge

---
+++ 3-challenges/findLogestSubstring.js
@@ -0,0 +1,17 @@
+// Find Longest Substring
+// Prompt:
+// Write a function called findLongestSubstring, which accepts a string and returns
+// the length of the longest substring with all distinct characters
+
+// Restate:
+// Given a string return the longest substring within with unique characters.
+// Inputs:
+// string
+// Outputs:
+// integer
+// Test I/O:
+// findLongestSubstring("alskdjfair") => 7
+// findLongersSubstring("alskdjf") => 7
+// findLongersSubstring("aaaaaa") => 1
+// Edge Cases:
+// If the length of the string is 0 return 0
