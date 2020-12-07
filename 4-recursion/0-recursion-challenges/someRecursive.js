7cd3dc43678a547c0c2f48842052380176be5ce7
Kanui <marley.kanui@gmail.com>
2020-12-07 00:14:26 -0600

    data-structures-algorithms - global commit 78 - Wrote pseudo code for someRecursiverecursive challenge

--- 4-recursion/0-recursion-challenges/someRecursive.js
+++ 4-recursion/0-recursion-challenges/someRecursive.js
@@ -16,3 +16,13 @@
 // someRecursive([1, 2, 3, 4, 5, 6, 7], () => isOdd?) => true
 // someRecursive([2, 4, 6], () => isOdd?) => false
 // someRecursive([1, 2, 3, 4, 5, 6, 7], (val) => val > 10 ? false : true) => false
+
+// Pseudo Code:
+// Return false if array.length is 0
+// initialize start pointer at 0
+// start internal helper function to handle the recursion
+// pass arrayHelper at i into the callback and store in a variable
+// if false return false
+// else increment start pointer and recursivley call helper again
+// if start > array.length - 1 return true
+// return recursiveHelper(array, callback)
