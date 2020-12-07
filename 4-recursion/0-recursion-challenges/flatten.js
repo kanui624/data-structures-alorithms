bb6d5b4918d67b6405a2ba98231000660dfcd916
Kanui <marley.kanui@gmail.com>
2020-12-07 11:20:16 -0600

    data-structures-algorithms - global commit 82 - Wrote solution flatten recursive challenge

--- 4-recursion/0-recursion-challenges/flatten.js
+++ 4-recursion/0-recursion-challenges/flatten.js
@@ -71,3 +71,18 @@
 // };
 
 // console.log(flatten([[1, 2], 3, [4]]));
+// My Attempt with the same strategy
+
+const flatten = (array) => {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      newArr = newArr.concat(flatten(array[i]));
+    } else {
+      newArr.push(array[i]);
+    }
+  }
+  return newArr;
+};
+
+console.log(flatten([1, [2, [3, 4]], [5], 6]));
