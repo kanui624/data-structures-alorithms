baf43baa2c4857e7df01e732d6c92eb57c652e0f
Kanui <marley.kanui@gmail.com>
2020-12-07 00:08:29 -0600

    data-structures-algorithms - global commit 77 - Wrote notes for someRecursiverecursive challenge

---
+++ 4-recursion/0-recursion-challenges/someRecursive.js
@@ -0,0 +1,18 @@
+// someRecursive
+// Prompt:
+// Write a function called someRecursive which accepts an array and a callback. The
+// function returns true if a single value in the array returns true when passed to
+// the callback. Otherwise it returns false
+
+// Restate:
+// Write a function the accepts an array and a callback function. The function returns
+// true if the only a single value in the array returns true when passed to the
+// callback. Else return false
+// Inputs:
+// an array and a callback function
+// Outputs:
+// Boolean
+// Test I/O:
+// someRecursive([1, 2, 3, 4, 5, 6, 7], () => isOdd?) => true
+// someRecursive([2, 4, 6], () => isOdd?) => false
+// someRecursive([1, 2, 3, 4, 5, 6, 7], (val) => val > 10 ? false : true) => false
