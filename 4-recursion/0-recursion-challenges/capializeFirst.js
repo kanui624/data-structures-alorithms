f775e2c5c7a9cd059d65b8813ef1349debea9286
Kanui <marley.kanui@gmail.com>
2020-12-07 11:36:36 -0600

    data-structures-algorithms - global commit 83 - Wrote notes for cap first recursive challenge

---
+++ 4-recursion/0-recursion-challenges/capializeFirst.js
@@ -0,0 +1,16 @@
+// Capitalize First - Recursion
+// Prompt:
+// Write a recursive function called CapitalizeFirst. Given an array of strings,
+// capitalize the first letter of each string in the array
+
+// Restate:
+// Write a function that takes in an array of strings and capitalizes the first
+// letter of each word within the string
+// Inputs:
+// a string with words within it
+// Outputs:
+// a new string with every first letter of each wi
+// Test I/O:
+// capitalizeFirst([car, taco, banana]) => [Car, Taco, Banana]
+// Edge Cases:
+// What if a string has multiple words in it?
