931f311d055da90fc81e3f3d65a9e9fceef50992
Kanui <marley.kanui@gmail.com>
2020-12-06 19:00:43 -0600

    data-structures-algorithms - global commit 74 - Wrote notes for isPalindrome recursive challenge

---
+++ 4-recursion/0-recursion-challenges/isPalindrome.js
@@ -0,0 +1,18 @@
+// isPalindrome - Recursion
+// Prompt:
+// Write a recursive function called isPalindrome which returns true if the string
+// passed to it is a palindrome. A palindrome is a word that reads to same forwards
+// and backwards
+
+// Restate:
+// Recursively write a function that checks if a given string is a palidrome
+// Inputs:
+// A single string
+// Outputs:
+// Boolean
+// Test I/O:
+// isPalindrome("abcdefg") => false
+// isPalindrome("abcdcba") => true
+// isPalindrome("racecar") => true
+// Edge Cases:
+// What if the given string's length is 0?
