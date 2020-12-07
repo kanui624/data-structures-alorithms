b3fb40090374653006b59c82d893fd792ee22d4e
Kanui <marley.kanui@gmail.com>
2020-12-07 13:40:29 -0600

    data-structures-algorithms - global commit 87 - Wrote pseudo Code for findLongestSubstring challenge

--- 3-challenges/findLogestSubstring.js
+++ 3-challenges/findLogestSubstring.js
@@ -15,3 +15,26 @@
 // findLongersSubstring("aaaaaa") => 1
 // Edge Cases:
 // If the length of the string is 0 return 0
+
+// Work Through:
+// "abcdefgabc" => 7
+//  i
+//  while(i < string.length)
+//  let map = {}
+//  if char in map
+//  map = {}
+//  arr[i] = true
+//  i++
+
+// Pseudo Code:
+// check if string's length is equal to 0, if so return 0
+// initialize an object to store elements in
+// initialize a pointer variable i = 0
+// initialize a count variable to 0
+// while i < string.length
+// if string[i] in object
+// set int to obj.length
+// set obj back to empty
+// and set obj[string[i]] = true
+// else
+// string[i] = true
