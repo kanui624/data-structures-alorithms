791e44343fdfff7949709a625553516fe69e9ae8
KANUI <marley.kanui@gmail.com>
2021-03-09 13:48:27 -0600

    data-structures-algorithms - global commit 98 - going over sort again

---
+++ 6-sorting-algorithms/bubble-sort-two.js
@@ -0,0 +1,25 @@
+// Bubble Sort Pseudo Code
+
+// Start looping from with a variable called i the end of the array towards the beginning
+// Start an inner loop with a variable called j from the beginning until i - 1
+// Compare if arr[j] is greater than arr[j + 1], swap their values
+// Return the sorted array afterwards
+
+// const bubbleSort = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array.length; j++) {
+//       if (array[j] > array[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return array;
+// };
+
+// console.log(bubbleSort([37, 45, 29, 8]));
+
+// const bubbleSort = () => {
+//   for (let i = array.length; i > 0; i--) {}
+// };
