76d093cf9372c89d7f057aff0c14ff63e17af4f4
KANUI <marley.kanui@gmail.com>
2021-03-31 10:37:58 -0500

    data-structures-algorithms - global commit 102 - going through sorting algorithms all day

--- 6-sorting-algorithms/bubble-sort-three.js
+++ 6-sorting-algorithms/bubble-sort-three.js
@@ -1,4 +1,30 @@
-// Implement Bubble Sort
+// // Implement Bubble Sort
+
+// const swap = (intOne, intTwo, array) => {
+//   let temp = array[intOne];
+//   array[intOne] = array[intTwo];
+//   array[intTwo] = temp;
+// };
+
+// const bubbleSort = (array) => {
+//   let isSorted = false;
+//   let counter = 0;
+//   while (!isSorted) {
+//     isSorted = true;
+//     for (let i = 0; i < array.length - 1 - counter; i++) {
+//       console.log(array[i], array[i + 1]);
+//       if (array[i] > array[i + 1]) {
+//         swap(i, i + 1, array);
+//         isSorted = false;
+//       }
+//     }
+//     counter++;
+//   }
+//   return array;
+// };
+
+// let bs = bubbleSort([1, 5, 3, 7, 5, 9, 7, 8, 67, 5, 6, 45]);
+// console.log(bs);
 
 const swap = (intOne, intTwo, array) => {
   let temp = array[intOne];
@@ -12,7 +38,6 @@ const bubbleSort = (array) => {
   while (!isSorted) {
     isSorted = true;
     for (let i = 0; i < array.length - 1 - counter; i++) {
-      console.log(array[i], array[i + 1]);
       if (array[i] > array[i + 1]) {
         swap(i, i + 1, array);
         isSorted = false;
@@ -23,5 +48,6 @@ const bubbleSort = (array) => {
   return array;
 };
 
-let bs = bubbleSort([1, 5, 3, 7, 5, 9, 7, 8, 67, 5, 6, 45]);
+let bs = bubbleSort([1, 4, 2, 6, 4, 8, 6, 7, 76, 5, 6, 454, 34, 43, 65, 8756]);
+
 console.log(bs);
